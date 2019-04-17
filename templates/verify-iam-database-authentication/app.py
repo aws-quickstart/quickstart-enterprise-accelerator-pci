383174a391e3a957e8d943b2b5519d7e45f30c35
Josh Joy <jjjoy@amazon.com>
2019-04-17 08:21:53 -0400

    remove regional certificates for example clean up verify database connection lambda

--- templates/verify-iam-database-authentication/app.py
+++ templates/verify-iam-database-authentication/app.py
@@ -13,20 +13,14 @@ rds_host  = os.environ["DatabaseEndpointURL"]
 rds_port = os.environ["DatabasePort"]
 user_name = os.environ["DatabaseUserName"]
 
-#certificate_file='rds-combined-ca-bundle.pem'
+#Please specify the region certificate to ensure that the connection is to the intended region.
+#Certificates available from here https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/UsingWithRDS.SSL.html#UsingWithRDS.SSL.IntermediateCertificates
 certificate_file='rds-ca-2015-root.pem'
-#certificate_file='rds-ca-2015-us-west-1.pem'
-#certificate_file='rds-ca-2015-us-west-2.pem'
 certificate_full_path=os.path.join(os.environ['LAMBDA_TASK_ROOT'],certificate_file)
 
 #https://dev.mysql.com/doc/mysql-security-excerpt/5.5/en/cleartext-pluggable-authentication.html
 #https://dev.mysql.com/doc/connector-python/en/connector-python-example-connecting.html
 #https://dev.mysql.com/doc/connector-python/en/connector-python-connectargs.html
-#https://aws.amazon.com/blogs/database/use-iam-authentication-to-connect-with-sql-workbenchj-to-amazon-aurora-mysql-or-amazon-rds-for-mysql/
-#Certificates available from here https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/UsingWithRDS.SSL.html#UsingWithRDS.SSL.IntermediateCertificates
-
-#conn = pymysql.connect(rds_host, user=user_name, passwd=token_database, db=db_name, connect_timeout=5, ssl={'ca':certificate_file})
-
 import mysql.connector
 def lambda_handler(event, context):
     try:
@@ -42,16 +36,3 @@ def lambda_handler(event, context):
     finally:
         cursor.close()
 
-'''
-import pymysql
-def lambda_handler(event,context):
-    try:
-        token_database=rds.generate_db_auth_token(rds_host,rds_port,user_name)
-        with closing(pymysql.connect(rds_host, user=user_name, passwd=token_database, db=db_name, connect_timeout=5, ssl={'ca':certificate_file},auth_plugin_map={'mysql_clear_password'})) as connection:
-            with connection.cursor() as cursor:
-                cursor.execute("SELECT 1")
-                return "Verified ability to establish IAM Database Authentication"
-    except:
-        logger.exception("ERROR: Unexpected error: Could not connect to MySQL instance.")
-        sys.exit()
-'''
