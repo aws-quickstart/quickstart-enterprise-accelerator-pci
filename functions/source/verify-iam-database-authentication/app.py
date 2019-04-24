66254ad6cfb80e3e85f067aa18829a3e0bc17c7e
Josh Joy <jjjoy@amazon.com>
2019-04-24 13:26:00 -0400

    download rds certificate clean up lambdas use pymsql

--- functions/source/verify-iam-database-authentication/app.py
+++ functions/source/verify-iam-database-authentication/app.py
@@ -13,26 +13,25 @@ rds_host  = os.environ["DatabaseEndpointURL"]
 rds_port = os.environ["DatabasePort"]
 user_name = os.environ["DatabaseUserName"]
 
-#Please specify the region certificate to ensure that the connection is to the intended region.
+#Please specify the region certificate to ensure that the connection is to the intended region. For now using the combined certificate bundle.
 #Certificates available from here https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/UsingWithRDS.SSL.html#UsingWithRDS.SSL.IntermediateCertificates
-certificate_file='rds-ca-2015-root.pem'
-certificate_full_path=os.path.join(os.environ['LAMBDA_TASK_ROOT'],certificate_file)
+s3 = boto3.client('s3')
+certificate_file='rds-combined-ca-bundle.pem'
+certificate_full_path=os.path.join('/tmp',certificate_file)
+s3.download_file('rds-downloads', 'rds-combined-ca-bundle.pem', certificate_full_path)
 
-#https://dev.mysql.com/doc/mysql-security-excerpt/5.5/en/cleartext-pluggable-authentication.html
-#https://dev.mysql.com/doc/connector-python/en/connector-python-example-connecting.html
-#https://dev.mysql.com/doc/connector-python/en/connector-python-connectargs.html
-import mysql.connector
+import pymysql
+
+try:
+    token_database=rds.generate_db_auth_token(rds_host,rds_port,user_name)
+    conn = pymysql.connect(rds_host, user=user_name, passwd=token_database, connect_timeout=5, ssl={'ca':certificate_full_path})
+except:
+    logger.exception("ERROR: Unexpected error: Could not connect to MySQL instance.")
+    sys.exit()
+
 def lambda_handler(event, context):
-    try:
-        cursor=None
-        token_database=rds.generate_db_auth_token(rds_host,rds_port,user_name)
-        with closing(mysql.connector.connect(user=user_name,password=token_database,host=rds_host,auth_plugin='mysql_clear_password',ssl_ca=certificate_full_path,ssl_verify_cert=True)) as connection:
-            cursor=connection.cursor()
-            cursor.execute("SELECT 1")
-            return "Verified ability to establish IAM Database Authentication"
-    except:
-        logger.exception("ERROR: Unexpected error: Could not connect to MySQL instance.")
-        sys.exit()
-    finally:
-        cursor.close()
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT 1;")
+        print(cursor.fetchone())
 
+    return "Verified ability to establish IAM Database Authentication"
